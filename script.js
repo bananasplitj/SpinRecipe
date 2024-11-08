594c22339836633975e73d0dc1672f7ff894de5e
bananasplitj <kimj1152@newschool.edu>
2024-11-08 07:16:08 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ function showPopup(type) {
   const popupText = document.getElementById("popup-text");
 
   // 팝업 내용 설정
- 
- if (type === "spin") {
+   if (type === "ingredients") {
+    popupText.innerHTML = `
+      1 pound penne rigate<br>
+      3 Tablespoons olive oil<br>
+      3 cloves garlic<br>
+      1/4 teaspoon crushed red pepper flakes (depending on how spicy you want it)<br>
+      1 28-ounce can whole peeled tomatoes (or 1 ½ cups fresh chopped tomatoes)
+    `;
+  } else if (type === "instructions") {
+    popupText.innerHTML = `
+      Cook pasta in a large pot of boiling water, according to package instructions, until tender.<br><br>
+      Meanwhile, heat olive oil in a large skillet over medium heat. Add garlic and crushed red pepper; cook, stirring for 30 seconds.<br><br>
+      Add tomatoes, crushing them roughly with the back of a wooden spoon, and tomato paste.<br><br>
+      Bring to a simmer over low heat and cook for 5-10 minutes. Remove from heat and add fresh chopped basil.<br><br>
+      When pasta is cooked, drain the water and add it to the sauce. Toss well. Taste and add more red pepper flakes or salt and pepper, if needed.<br><br>
+      Serve immediately topped with a generous portion of grated pecorino or parmesan cheese and fresh chopped parsley.
+    `;
+ } else if (type === "spin") {
     popupText.textContent = "Spinning for a new recipe!";
   }
 
